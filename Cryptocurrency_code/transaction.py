e2fbaa7f0318ed8e06431f16015258296a520dcd
eilonnave <43199234+eilonnave@users.noreply.github.com>
2019-03-04 12:14:33 +0200

    Add files via upload

--- Cryptocurrency_code/transaction.py
+++ Cryptocurrency_code/transaction.py
@@ -41,13 +41,32 @@ class Transaction:
         for the sender
         :returns: the encrypted message
         """
-        return recipient_public_key.encrypt(self.to_string(), 32)
+        return recipient_public_key.encrypt(self.to_string(), 32)[0]
 
-    def sign_transaction(self):
-        pass
+    def sign_transaction(self, sender_private_key):
+        """
+        the function signs the transaction
+        using the sender private key
+        :param sender_private_key: the server's private
+        key
+        :return: the signature that created from
+        the key and the transaction
+        """
+        return sender_private_key.sign(self.hash, '')
 
 
 if __name__ == "__main__":
     t_1 = Transaction('1', '1', 1)
     t_2 = Transaction('2', '2', 2)
     assert t_1.hash != t_2.hash
+    private_key_1 = RSA.generate(2048)
+    public_key_1 = private_key_1.publickey()
+    private_key_2 = RSA.generate(2048)
+    public_key_2 = private_key_2.publickey()
+    encrypted_transaction = t_1.encrypt_transaction(public_key_2)
+    decrypted_transaction = private_key_2.decrypt(encrypted_transaction)
+    assert decrypted_transaction == t_1.to_string()
+    decrypted_transaction = private_key_1.decrypt(encrypted_transaction)
+    assert decrypted_transaction != t_1.to_string()
+    signature = t_1.sign_transaction(private_key_1)
+    assert public_key_1.verify(t_1.hash, signature)

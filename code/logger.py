7a5f6ed661e6f0aa011f0935b06bfe846284d273
eilonnave <eilon.nave@gmail.com>
2019-03-28 15:36:06 +0200

    28/3/2019- finish signature implemantions

--- code/logger.py
+++ code/logger.py
@@ -1,14 +1,27 @@
 # -*- coding: utf-8 -*-
 import logging
+import os
 LOG_FORMAT = '%(levelname)s | %(asctime)s | %(processName)s | %(message)s'
 LOG_LEVEL = logging.DEBUG
+LOG_DIR = 'log'
 
 
-class Logger(logging):
+class Logger():
     def __init__(self, file_name):
         """
         constructor
         """
-        self.basicConfig(format=LOG_FORMAT,
-                         filename=file_name,
-                         level=LOG_LEVEL)
+        if not os.path.isdir(LOG_DIR):
+            os.makedirs(LOG_DIR)
+        logging.basicConfig(format=LOG_FORMAT,
+                            filename=LOG_DIR+'/'+file_name,
+                            level=LOG_LEVEL)
+
+    @staticmethod
+    def info(message):
+        """
+        the function adds info message to the
+        logging
+        :param message: the message to logging
+        """
+        logging.info(message)
